ccc2d38b682fd22ac8e85a1d64a0f15b96e92b3d
sto <son.c.to@gmail.com>
2019-04-13 14:43:55 +0800

    drawer

--- src/cljs/cdr/mdc.cljs
+++ src/cljs/cdr/mdc.cljs
@@ -16,8 +16,7 @@
 (def tab-bar (r/create-class
               {:component-did-mount (fn [this]
                                       (let [element (dom/dom-node this)]
-                                        (js/mdc.tabBar.MDCTabBar. element))
-                                      )
+                                        (js/mdc.tabBar.MDCTabBar. element)))
                :reagent-render
                (fn [& params]
                  [:div {:class "mdc-tab-bar", :role "tablist"}
@@ -35,3 +34,34 @@
                        [:span {:class "mdc-tab-indicator__content mdc-tab-indicator__content--underline"}]]
                       [:span {:class "mdc-tab__ripple"}]]]]]]
                  )}))
+
+(def drawer (r/create-class
+             {:component-did-mount (fn [this]
+
+                                     (let [element (js/document.querySelector ".mdc-drawer")]
+                                       (prn "mount " element)
+                                       (js/mdc.drawer.MDCDrawer.attachTo element)))
+              :reagent-render
+              (fn [& params]
+                [:div
+                 [:aside {:class "mdc-drawer mdc-drawer--dismissible"}
+                  [:div {:class "mdc-drawer__content"}
+                   [:div {:class "mdc-list"}
+                    [:a {:class "mdc-list-item mdc-list-item--activated", :href "#", :aria-current "page"}
+                     [:i {:class "material-icons mdc-list-item__graphic", :aria-hidden "true"} "inbox"]
+                     [:span {:class "mdc-list-item__text"} "Inbox"]]
+                    [:a {:class "mdc-list-item", :href "#"}
+                     [:i {:class "material-icons mdc-list-item__graphic", :aria-hidden "true"} "send"]
+                     [:span {:class "mdc-list-item__text"} "Outgoing"]]
+                    [:a {:class "mdc-list-item", :href "#"}
+                     [:i {:class "material-icons mdc-list-item__graphic", :aria-hidden "true"} "drafts"]
+                     [:span {:class "mdc-list-item__text"} "Drafts"]]]]]
+                 [:div {:class "mdc-drawer-app-content"}
+                  [:header {:class "mdc-top-app-bar app-bar", :id "app-bar"}
+                   [:div {:class "mdc-top-app-bar__row"}
+                    [:section {:class "mdc-top-app-bar__section mdc-top-app-bar__section--align-start"}
+                     [:a {:href "#", :class "demo-menu material-icons mdc-top-app-bar__navigation-icon"} "menu"]
+                     [:span {:class "mdc-top-app-bar__title"} "Dismissible Drawer"]]]]
+                  [:main {:class "main-content", :id "main-content"}
+                   [:div {:class "mdc-top-app-bar--fixed-adjust"} "App Content"]]]]
+                )}))
