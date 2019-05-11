4a7939ffab97399c7677894d80d53867edf9623f
sto <son.c.to@gmail.com>
2019-05-11 23:17:22 +0800

    refactor

--- src/cljs/cdr/git.cljs
+++ src/cljs/cdr/git.cljs
@@ -16,4 +16,32 @@
 (comment
   (clone {:url "https://github.com/sonwh98/cdr.git"
           :dir "/cdr" })
+  
+  (a/go
+    (let [dir "/cdr2"]
+      (prn "1->" (a/<! (await (js/window.pfs.mkdir dir))))
+      (prn "2->" (a/<! (await (js/window.pfs.readdir dir))))
+      (prn "3->" (a/<! (await (js/git.clone #js{:dir "/cdr2"
+                                                :corsProxy "https://cors.isomorphic-git.org"
+                                                :url "https://github.com/sonwh98/cdr.git"
+                                                :ref "master"
+                                                :singleBranch true
+                                                :depth 10}))))
+      (prn "4->" (a/<! (await (js/window.pfs.readdir dir))))
+      (prn "4->" (a/<! (await (js/window.pfs.readFile (str dir "/src/clj/user.clj")))))
+      ))
+
+  (a/go
+    (prn (a/<! (await (js/git.log #js{:dir "/cdr2"}))))
+    (let [[err buff] (a/<! (await (js/window.pfs.readFile "/cdr2/src/clj/user.clj")))]
+      (prn "user.clj=" (array-buffer->str  buff ) ))
+    )
+
+  (a/go
+    (let [[err files] (a/<! (await (js/git.listFiles #js{:dir "/cdr2/"
+                                                         :ref "HEAD"})))]
+      (prn err)
+      (prn files)
+      ))
+
   )
--- src/cljs/cdr/util.cljs
+++ src/cljs/cdr/util.cljs
@@ -17,3 +17,22 @@
 
 (defn in? [e coll]
   (boolean (some #(= % e) coll)))
+
+(defn cmp [b a]
+  (let [b-count (count b)
+        a-count (count a)
+        c (if (> b-count a-count)
+            (map (fn [[i a-element]]
+                   (let [b-element (b i)]
+                     (compare b-element a-element)))
+                 (map-indexed (fn [i item] [i item]) a))
+            (map (fn [[i b-element]]
+                   (let [a-element (a i)]
+                     (compare b-element a-element)))
+                 (map-indexed (fn [i item] [i item]) b)))
+        c   (some (fn [v] (if (= 0  v)
+                            0
+                            v
+                            )) c)]
+
+    c))
