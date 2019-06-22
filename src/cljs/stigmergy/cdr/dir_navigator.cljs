4a067e146513f9f16cbbf7e5f86a326313cfb095
sto <son.c.to@gmail.com>
2019-06-22 21:31:43 +0800

    rm let

--- src/cljs/stigmergy/cdr/dir_navigator.cljs
+++ src/cljs/stigmergy/cdr/dir_navigator.cljs
@@ -22,26 +22,25 @@
     (on-context-menu x y)))
 
 (defn dir [{:keys [node on-click on-context-menu] :as args}]
-  (let []
-    [:li
-     [:span {:class "dir"
-             :on-click #(toggle node %)
-             :on-context-menu  #(context-menu-handler % on-context-menu)}
-      (get-name @node)]
-     [:ul {:class (if (:visible? @node)
-                    "sub-dir active"
-                    "sub-dir")
-           :style {:list-style-type :none}}
-      (let [index-children (-> @node get-children tily/with-index)]
-        (doall (for [[index c] index-children
-                     :let [k (-> @node keys first)
-                           child (r/cursor node [k index])]]
-                 (with-meta (if-let [file-name (:name c)]
-                              [:li {:on-click #(on-click c)
-                                    :on-context-menu  #(context-menu-handler % on-context-menu)}
-                               file-name]
-                              [dir (merge args {:node child}) ])
-                   {:key (str c)}))))]]))
+  [:li
+   [:span {:class "dir"
+           :on-click #(toggle node %)
+           :on-context-menu  #(context-menu-handler % on-context-menu)}
+    (get-name @node)]
+   [:ul {:class (if (:visible? @node)
+                  "sub-dir active"
+                  "sub-dir")
+         :style {:list-style-type :none}}
+    (let [index-children (-> @node get-children tily/with-index)]
+      (doall (for [[index c] index-children
+                   :let [k (-> @node keys first)
+                         child (r/cursor node [k index])]]
+               (with-meta (if-let [file-name (:name c)]
+                            [:li {:on-click #(on-click c)
+                                  :on-context-menu  #(context-menu-handler % on-context-menu)}
+                             file-name]
+                            [dir (merge args {:node child}) ])
+                 {:key (str c)}))))]])
 
 (defn tree [{:keys [node] :as args}]
   [:ul {:style {:list-style-type :none
