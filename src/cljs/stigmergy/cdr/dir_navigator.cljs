4dd5f441c6631fc29d8998df90ca9b7f8afe67e2
sto <son.c.to@gmail.com>
2019-06-23 23:23:39 +0800

    set selected-node

--- src/cljs/stigmergy/cdr/dir_navigator.cljs
+++ src/cljs/stigmergy/cdr/dir_navigator.cljs
@@ -1,7 +1,8 @@
 (ns stigmergy.cdr.dir-navigator
   (:require [stigmergy.tily :as tily]
             [reagent.core :as r]
-            [taoensso.timbre :as log :include-macros true]))
+            [taoensso.timbre :as log :include-macros true]
+            [stigmergy.cdr.state :as state]))
 
 (defn toggle [node evt]
   (let [element (.-target evt)
@@ -22,25 +23,32 @@
     (on-context-menu x y)))
 
 (defn dir [{:keys [node on-click on-context-menu] :as args}]
-  [:li
-   [:span {:class "dir"
-           :on-click #(toggle node %)
-           :on-context-menu  #(context-menu-handler % on-context-menu)}
-    (get-name @node)]
-   [:ul {:class (if (:visible? @node)
-                  "sub-dir active"
-                  "sub-dir")
-         :style {:list-style-type :none}}
-    (let [index-children (-> @node get-children tily/with-index)]
-      (doall (for [[index c] index-children
-                   :let [k (-> @node keys first)
-                         child (r/cursor node [k index])]]
-               (with-meta (if-let [file-name (:name c)]
-                            [:li {:on-click #(on-click c)
-                                  :on-context-menu  #(context-menu-handler % on-context-menu)}
-                             file-name]
-                            [dir (merge args {:node child}) ])
-                 {:key (str c)}))))]])
+  (let [select-node #(swap! state/app-state assoc :selected-node %)]
+    [:li
+     [:span {:class "dir"
+             :on-click #(do
+                          (select-node @node)
+                          (toggle node %))
+             :on-context-menu  #(context-menu-handler % on-context-menu)}
+      (get-name @node)]
+     [:ul {:class (if (:visible? @node)
+                    "sub-dir active"
+                    "sub-dir")
+           :style {:list-style-type :none}}
+      (let [index-children (-> @node get-children tily/with-index)]
+        (doall (for [[index c] index-children
+                     :let [k (-> @node keys first)
+                           child (r/cursor node [k index])]]
+                 (with-meta (if-let [file-name (:name c)]
+                              [:li {:on-click #(do
+                                                 (select-node c)
+                                                 (on-click c))
+                                    :on-context-menu #(do
+                                                        (select-node c)
+                                                        (context-menu-handler % on-context-menu))}
+                               file-name]
+                              [dir (merge args {:node child}) ])
+                   {:key (str c)}))))]]))
 
 (defn tree [{:keys [node] :as args}]
   [:ul {:style {:list-style-type :none
